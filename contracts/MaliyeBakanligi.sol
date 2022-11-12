f16005c38a4a91680eb7288382625689bdbc86a5
Bilal Basut <bilalbasut@gmail.com>
2022-11-12 21:06:08 +0300

    internal func added

--- contracts/MaliyeBakanligi.sol
+++ contracts/MaliyeBakanligi.sol
@@ -4,12 +4,12 @@ pragma solidity >=0.8.0;
 import "./Kurum.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract SaglikBakanligi is Kurum {
+contract MaliyeBakanligi is Kurum {
     constructor(string memory _name, address _signer) Kurum(_name, _signer) {}
 
-    mapping(uint => bytes) receteNumberToSignature;
+    mapping(uint => bytes) cekNumberToSig;
 
-    function verify(bytes memory signature, bytes32 messageHash) public view returns (address recovered) {
+    function verify(bytes memory signature, bytes32 messageHash) external view returns (address recovered) {
         recovered = ECDSA.recover(messageHash, signature);
         require(authorized[recovered] != 0, "address not found");
     }
@@ -19,8 +19,7 @@ contract SaglikBakanligi is Kurum {
         require(authorized[recovered] != 0, "address not found");
     }
 
-    function receteExecution(bytes memory signature, bytes32 messageHash) public returns(bool) {
-        require(!usedSignatures[signature]);
+    function cekExecution(bytes memory signature, bytes32 messageHash) public returns(bool) {
         require(authorized[msg.sender] == 2, "caller is not a pharmacy"); // 2 for pharmacy
         address recovered = internalVerify(signature, messageHash);
         require(authorized[recovered] == 1, "signer is not a doctor"); // 1 for doctor
@@ -28,8 +27,8 @@ contract SaglikBakanligi is Kurum {
         return usedSignatures[signature];
     }
 
-    function publishRecete(uint receteNo, bytes memory signature) external {
-        require(authorized[msg.sender] == 1, "caller is not a pharmacy");
-        receteNumberToSignature[receteNo] = signature;
+    function publishCek(uint cekNo, bytes memory signature) external {
+        require(authorized[msg.sender] == 1, "caller is not valid");
+        cekNumberToSig[cekNo] = signature;
     }
 }
