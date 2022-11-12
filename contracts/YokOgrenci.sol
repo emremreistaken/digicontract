c551b95043ab071b7d2a25a2b5e9fa5320c88a36
Bilal Basut <bilalbasut@gmail.com>
2022-11-12 14:48:17 +0300

    YokOgrenci added

--- contracts/YokOgrenci.sol
+++ contracts/YokOgrenci.sol
@@ -3,22 +3,18 @@ pragma solidity >=0.8.0;
 
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract eImza is Kurum {
+contract YokOgrenci is Kurum {
 
     constructor(string memory _name, address _signer) Kurum(_name, _signer) {}
 
     mapping(bytes32 => bytes32) signatureToHash;
 
-    function internalVerify(bytes32 memory signature, bytes32 memory messageHash) internal pure returns(address) {
-        return ECDSA.recover(messageHash, signature);
+    function verify(bytes32 memory signature, bytes32 memory messageHash) external  returns(bool) {
+        require(authorized[(ECDSA.recover(messageHash, signature))]);
+
     }
 
     function sign(bytes32 memory signature, bytes32 memory messageHash) external {
         signatureToHash[signature] = messageHash;
     }
-
-    function verify(bytes32 memory signature, bytes32 memory messageHash) external {
-        require(internalVerify(signature, messageHash) == );
-    }
-
 }
