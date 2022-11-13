7cbe99afb09fb70a8520bbf3aa24e5a60db21dec
onuruci <onur.yuce@metu.edu.tr>
2022-11-13 08:38:31 +0300

    new verify

--- contracts/SaglikBakanligi.sol
+++ contracts/SaglikBakanligi.sol
@@ -3,16 +3,12 @@ pragma solidity >=0.8.0;
 
 import "./Kurum.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 
-contract SaglikBakanligi is Kurum {
-    constructor(string memory _name, address _signer) Kurum(_name, _signer) {}
+contract SaglikBakanligi is Kurum, EIP712 {
+    constructor(string memory _name, string memory version, address _signer) Kurum(_name, _signer) EIP712(_name, version) {}
 
     mapping(uint => bytes) public receteNumberToSignature;
-
-    function verify(bytes memory signature, bytes32 messageHash) external view returns (address recovered) {
-        recovered = ECDSA.recover(messageHash, signature);
-        require(authorized[recovered] != 0, "address not found");
-    }
 
     function internalVerify(bytes memory signature, bytes32 messageHash) internal view returns (address recovered) {
         recovered = ECDSA.recover(messageHash, signature);
@@ -33,4 +29,25 @@ contract SaglikBakanligi is Kurum {
         receteNumberToSignature[receteNo] = signature;
         userToId[msg.sender].push(receteNo);
     }
+
+    function verify(bytes memory signature, string memory name, string memory value) external view returns (address recovered) {
+
+
+        bytes32 hashed = _hashTypedDataV4(keccak256(
+            abi.encode(
+                keccak256(
+                    "check(string name,string value)"
+                ),
+                keccak256(bytes(name)),
+                keccak256(bytes(value))
+            )
+        ));
+
+        return ECDSA.recover(hashed, signature);
+    }
+
+    function getChainID() view public returns(uint256 _b) {
+        return block.chainid;
+    }
+    
 }
